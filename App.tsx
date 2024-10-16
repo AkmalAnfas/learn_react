e3ac5dfb2929acb98f122eb19775765a2d42b13d
Akmal Anfas <akmalanfas7@gmail.com>
2024-10-16 16:35:18 +0530

    Typscript Button close | interface

--- App.tsx
+++ App.tsx
@@ -1,8 +1,93 @@
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
-function App() {
-  return <div><ListGroup/></div>;
 
+function App(){
+  const [alertVisible, setAlertVisibility]   = useState(false);
+
+  return(
+    <div>
+      {alertVisible && <Alert onClose={()=>setAlertVisibility(false)}>My alert</Alert>}
+
+      <Button color="primary" onClick={() =>setAlertVisibility(true) }>
+      My Button   
+      </Button>
+      
+
+    </div>
+  );
 }
 
-export default App; 
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import ListGroup from "./components/ListGroup";
+
+//function App() {
+  //let items=['New York','San Francisco','Tokyo','London','Paris'];
+
+  //const handleSelectItem = (item: string) => {
+    //console.log(item);
+  //} 
+
+  //return (
+  //<div>
+    //<ListGroup items={items} heading= "cities" onSelectItem={handleSelectItem}/>
+  //</div>
+//);
+
+//}
+
+//export default App; 
